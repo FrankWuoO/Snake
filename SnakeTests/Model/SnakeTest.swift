4e36bd19749d2690e9d70a024683aadf83f575e5
cheng-en wu <>
2020-04-20 13:27:15 +0800

    remove useless test functions.

--- SnakeTests/Model/SnakeTest.swift
+++ SnakeTests/Model/SnakeTest.swift
@@ -16,14 +16,6 @@ class SnakeTest: XCTestCase {
     lazy var testSnake: SnakeAbility&SnakeAction = Snake(bounds: testBounds, length: testLength)
     //[0, 0] [1, 0] [2, 0] [3, 0]
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testInit() {
         XCTAssert(testSnake.body.count == testLength)
     }
